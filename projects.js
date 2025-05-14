85bf25c13a053285102b415481c4bb22bd0dbf88
serolyn <walidbenabdelkrimelfilali@gmail.com>
2025-05-14 16:48:41 +0000

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,8 +1,6 @@
 
-
 const projects = [
-  { repo:"serolyn/ia-psychologie-2025",
-    tagline:"Recherche exploratoire sur les dynamiques émotionnelles homme–IA (rapport PDF)." },
-  { repo:"serolyn/DATAMETEOPARIS",
-    tagline:"Datavisualisation et analyse de 35 000 lignes météo (Python + figures)." }
+  { repo:"serolyn/ia-psychologie-2025", tagline:"…" },
+  { repo:"serolyn/DATAMETEOPARIS", tagline:"…" }
 ];
+
