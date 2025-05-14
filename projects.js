702f8568a80c608528d19d69751dc87479cd6590
serolyn <walidbenabdelkrimelfilali@gmail.com>
2025-05-14 16:15:24 +0000

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,13 @@
+
+const projects = [
+  {
+    title: "IA & Psychologie",
+    desc: "Recherche exploratoire sur les dynamiques émotionnelles homme–IA (rapport PDF).",
+    link: "https://github.com/serolyn/ia-psychologie-2025"
+  },
+  {
+    title: "Climat urbain Paris",
+    desc: "Datavisualisation et analyse de 35 000 lignes météo (Python + figures).",
+    link: "https://github.com/serolyn/DATAMETEOPARIS"
+  }
+];
