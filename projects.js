53e3c13e3f60af0d560e5ff16a72c012524ac4b7
serolyn <walidbenabdelkrimelfilali@gmail.com>
2025-05-18 17:37:45 +0000

    Update projects.js

--- projects.js
+++ projects.js
@@ -5,7 +5,7 @@ const projects = [
   {
     repo:    "serolyn/DATAMETEOPARIS",
     tagline: "Notebook d’analyse météo 1980-2024, TM annuelle pondérée.",
-    cover:   "assets/climat_cover.png",
+    cover:   "Climat.png",
     link:    "https://nbviewer.org/github.com/serolyn/DATAMETEOPARIS/blob/main/climat_paris.ipynb"
   },
   {
